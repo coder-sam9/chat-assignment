2b8b52987337f614674ae308e71edb694e74e251
coder-sam9 <simplysam679@gmail.com>
2025-03-21 21:38:59 +0530

    Remove sensitive Firebase configuration from source code

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,14 +4,7 @@ import { getAuth, signInAnonymously } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // 🔐 Firebase project credentials extracted from google-services.json
-const firebaseConfig = {
-  apiKey: "AIzaSyAT-Zi19UUgHtrtdg73VFQ4ekTVrFflyw8",
-  authDomain: "chatapp-57728.firebaseapp.com",  // Match project ID
-  projectId: "chatapp-57728",  // Project ID
-  storageBucket: "chatapp-57728.appspot.com",  // Match project ID
-  messagingSenderId: "526540405150",  // Messaging sender ID
-  appId: "1:526540405150:android:19a967f997ee17761bbe8f"  // Android app ID
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
